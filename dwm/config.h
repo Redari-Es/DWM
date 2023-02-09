b8eb3dc65f601a42b7c9533d300d18f9140844a9
Redari-Es <905422030@qq.com>
2023-02-09 19:23:04 +0800

    dwm

--- dwm/config.h
+++ dwm/config.h
@@ -5,18 +5,19 @@
 /* appearance */
 static const unsigned int borderpx = 3;       /* border pixel of windows */
 static const unsigned int snap = 32;          /* snap pixel */
-static const unsigned int systraypinning = 0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2; /* systray spacing */
+static const unsigned int systraypinning = 1; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 4; /* systray spacing */
 static const int systraypinningfailfirst = 1; /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray = 1;             /* 0 means no systray */
 static const unsigned int gappih = 16;        /* horiz inner gap between windows */
 static const unsigned int gappiv = 12;        /* vert inner gap between windows */
-static const unsigned int gappoh = 14;        /* horiz outer gap between windows and screen edge */
+static const unsigned int gappoh = 18;        /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov = 14;        /* vert outer gap between windows and screen edge */
-static const int smartgaps = 1;               /* 1 means no outer gap when there is only one window */
+static const int smartgaps = 0;               /* 1 means no outer gap when there is only one window */
 static const int showbar = 1;                 /* 0 means no bar */
 static const int topbar = 1;                  /* 0 means bottom bar */
-static const Bool viewontag = True;           /* Switch view on tag switch */
+//
+static const Bool viewontag = True; /* Switch view on tag switch */
 static const char* fonts[] = { "SauceCodePro Nerd Font Mono:size=16" };
 static const char dmenufont[] = "SauceCodePro Nerd Font Mono:size=16";
 static const char col_gray1[] = "#222222";
@@ -64,7 +65,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact = 0.5;   /* factor of master area size [0.05..0.95] */
+static const float mfact = 0.5;   /* factor of master area size [0.05..0.95]..0.95] */
 static const int nmaster = 1;     /* number of clients in master area */
 static const int resizehints = 1; /* 1 means respect size hints in tiled resizals */
 

04a7794183d4e55347113ae4e5460e5050ecd984
Redari-Es <905422030@qq.com>
2023-02-09 19:21:12 +0800

    add the multi screen wallpaper

--- scripts/wp-change.sh
+++ scripts/wp-change.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-feh --recursive --randomize --bg-fill ~/Pictures/wallpapers/*
-#feh --recursive --randomize --bg-fill ~/Pictures/wallpapers/view
+## 一个壁纸一个屏幕
+#feh --recursive --randomize --bg-fill ~/Pictures/wallpapers/*
+## 一个壁纸多个屏幕
+#feh --recursive --randomize --bg-fill --no-xinerama ~/Pictures/wallpapers/*
+## 多个壁纸多个屏幕
+feh --recursive --randomize --bg-fill ~/Pictures/wallpapers/* ~/Pictures/wallpapers/*
+
